10b6842a84b4d79535e97714607fd020fc3a1875
TAMADA <a388087@gmail.com>
2022-06-16 14:14:10 +0800

    修复vpu类is_run_ 名字被改

--- medialib/media_func/decode/vpu_decode.h
+++ medialib/media_func/decode/vpu_decode.h
@@ -25,12 +25,13 @@ public:
 
     static void decode_func(void* in_this);
     std::thread* m_decode_func_thread= nullptr;
-    bool m_running= false;
     ffmpeg_pull_flow_param_t* m_ffmpeg_param= nullptr;
     rk1808_flow_param_t* m_param= nullptr;
     CONDITION_VARIALBE_t m_cv;      //条件等待变量
     bool m_pkt_done= false;
     UINT8* m_yuv420p= nullptr;
+private:
+    bool is_run_= false;
 };
 #endif
 
